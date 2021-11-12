42b120dad7251506750aaa543eef592ec18ce83e
MH-RaZa-Developer <webdevelopermehedi1@gmail.com>
2021-11-12 19:55:13 +0600

    Done

--- src/Shared/ProductCard/ProductCard.js
+++ src/Shared/ProductCard/ProductCard.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './ProductCard.css';
+import React from "react";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./ProductCard.css";
+import Zoom from "react-reveal/Zoom";
 
 const ProductCard = ({ car }) => {
-    
-    const { _id, name, image, description, price } = car;
+  const { _id, name, image, description, price } = car;
 
-    return (
-        <Col>
-            <div className="single-products">
-            <img width="100%" src={image} alt="" />
-            <div className="product-des">
-                <h3>{name}</h3>
-                <p>{description.slice(0, 80)}</p>
-                <p className="price">$ {price}</p>
-                    <Link to={`/placeOrder/${_id}`}>
-                    <button className="regular-btn">Buy Now <i className="fas fa-arrow-right"></i></button>
-                    </Link>
-            </div>
+  return (
+    <Zoom top>
+      <Col>
+        <div className="single-products">
+          <img width="100%" src={image} alt="" />
+          <div className="product-des">
+            <h3>{name}</h3>
+            <p>{description.slice(0, 80)}</p>
+            <p className="price">$ {price}</p>
+            <Link to={`/placeOrder/${_id}`}>
+              <button className="regular-btn">
+                Buy Now <i className="fas fa-arrow-right"></i>
+              </button>
+            </Link>
+          </div>
         </div>
-        </Col>
-    );
+      </Col>
+    </Zoom>
+  );
 };
 
 export default ProductCard;
--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './Banner.css';
+import Fade from 'react-reveal/Fade';
 
 const Banner = () => {
     return (
         <div className="banner-area">
             <Container>
                 <Row className="banner-intro">
+                    <Fade left>
                     <Col xs={12} md={6} lg={6}>
                         <h1>Car Deal is the website that sell wonderful car for you!</h1>
                         <p>A car (or automobile) is a wheeled motor vehicle used for transportation. Most definitions of cars say that they run primarily on roads, seat one-to-eight people, have four wheels and mainly transport people rather than goods.</p>
                         <button className="readBtn">Read More</button>
                     </Col>
+                    </Fade>
                     <Col xs={12} md={6} lg={6}>
 
                     </Col>
